2c3dfd9219ae30325d122a1b73d11ec668e8ab24
Shaily Priya <91407027+shailyypriyaa@users.noreply.github.com>
2023-10-13 22:16:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+const accessKey = "7VK2z3_vAw-sD8b2d4brpOM5XM9ID0DkcsNUem2HF0c";
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+
+async function searchImages(){
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+    // created a dynamic url
+
+    const response = await fetch(url)
+    const data = await response.json();
+
+    const results = data.results;
+    
+    if(page === 1)
+    {
+        searchResults.innerHTML = ""
+    }
+
+    results.map((result) =>{
+        const imageWrapper = document.createElement('div');
+        imageWrapper.classList.add("search-result")
+        const image = document.createElement('img')
+        image.src = result.urls.small
+        image.alt = result.alt_description
+        const imageLink = document.createElement('a')
+        imageLink.href = result.links.html
+        imageLink.target = "_blank"
+        imageLink.textContent = result.alt_description
+
+        imageWrapper.appendChild(image)
+        imageWrapper.appendChild(imageLink)
+        searchResults.appendChild(imageWrapper)
+    });
+
+    page++
+    if(page > 1)
+    {
+        showMore.style.display = "block";
+    }
+}
+
+formEl.addEventListener("submit", (event) =>{
+    event.preventDefault()
+    // When you call event.preventDefault(), you are telling the browser not to execute the default action associated with the event 
+    page = 1
+    searchImages();
+})
+
+showMore.addEventListener("click", ()=>{
+    searchImages();
+})
